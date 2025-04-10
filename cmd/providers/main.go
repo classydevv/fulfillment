627d98f93a3f58993023ac1665735399523570b5
Pavel Opryshko <p.oprychko@gmail.com>
2025-04-10 10:12:03 +0300

    proto api providers generated

--- cmd/providers/main.go
+++ cmd/providers/main.go
@@ -1,32 +1,25 @@
 package main
 
 import (
-	"net/http"
-	"os"
+	"sync"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	pb "github.com/classydevv/metro-fulfillment/pkg/api/providers"
 )
+
+type providerId string
+
+type server struct {
+	pb.UnimplementedNotesServiceServer
+	
+	mu sync.RWMutex
+	providers map[providerId]*pb.Provider
+}
+
+func NewServer() *server {
+	return &server{providers: make(map[providerId]*pb.Provider)}
+}
 
 func main() {
 
-	e := echo.New()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3")
-	})
-
-	e.GET("/health", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	})
-
-	httpPort := os.Getenv("PORT")
-	if httpPort == "" {
-		httpPort = "8080"
-	}
-
-	e.Logger.Fatal(e.Start(":" + httpPort))
+	
 }
